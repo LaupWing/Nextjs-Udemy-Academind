ee88ff286020f20cc07b9d4eb86d37c58502d12b
LaupWing <loc_strict@hotmail.com>
2022-01-07 15:41:47 +0100

    75

--- nextjs-course/components/events/event-item.js
+++ nextjs-course/components/events/event-item.js
@@ -1,4 +1,8 @@
 import Link from "next/link"
+import AddressIcon from "../icons/address-icon"
+import ArrowRightIcon from "../icons/arrow-right-icon"
+import DateIcon from "../icons/date-icon"
+import Button from "../ui/button"
 import classes from './event-item.module.css'
 
 const EventItem = ({title, image, date, location, id}) => {
@@ -18,14 +22,19 @@ const EventItem = ({title, image, date, location, id}) => {
             <div className={classes.summary}>
                <h2>{title}</h2>
                <div className={classes.date}>
+                  <DateIcon/>
                   <time>{transform_date}</time>
                </div>
                <div className={classes.address}>
+                  <AddressIcon/>
                   <address>{formatted_address}</address>
                </div>
             </div>
-            <div className={classes.action}>
-               <Link href={explor_link}>Explore Event</Link>
+            <div className={classes.actions}>
+               <Button link={explor_link}>
+                  <span><ArrowRightIcon/></span>
+                  Explore Event
+               </Button>
             </div>
          </div>
       </li>
