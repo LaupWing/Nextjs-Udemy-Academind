2a2e840655abf1b310d30f95cfe2db2ca746dc08
LaupWing <loc_strict@hotmail.com>
2022-01-13 15:46:48 +0100

    91

--- get_static_props/pages/index.js
+++ get_static_props/pages/index.js
@@ -1,4 +1,9 @@
+import fs from 'fs/promises'
+import path from 'path'
+
 export default function Home() {
+   // const { products } = props
+
    return (
       <ul>
          <li>Product 1</li>
@@ -6,4 +11,17 @@ export default function Home() {
          <li>Product 3</li>
       </ul>
    )
+}
+
+export async function getStaticProps(){
+   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json') 
+   const jsonData = await fs.readFile(filePath)
+   const data = JSON.parse(jsonData)
+   console.log(data)
+
+   return {
+      props: {
+         products: [{id: 'p1', title: 'Product 1'}]
+      }
+   }
 }
